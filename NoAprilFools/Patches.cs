4eaa7bb1e3ebf43ea89566130e8130907006c693
Dane "Dev <81720436+developer9998@users.noreply.github.com>
2025-04-01 12:51:56 -0230

    Minor changes

---
+++ NoAprilFools/Patches.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NoAprilFools
+{
+    [HarmonyPatch]
+    public class Patches
+    {
+        [HarmonyPatch(typeof(AprilFools), nameof(AprilFools.GenerateSmoothTarget)), HarmonyPrefix]
+        public static bool AprilFoolsPatch2023(ref float __result)
+        {
+            __result = 1f;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(ScalerUpper), "Update"), HarmonyPrefix]
+        public static bool AprilFoolsPatch2025(ScalerUpper __instance, Transform[] ___target)
+        {
+            if (___target != null && ___target.Length == 1 && ___target[0].name.ToLower().StartsWith("head"))
+            {
+                __instance.enabled = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- NoAprilFools/Plugin.cs
+++ NoAprilFools/Plugin.cs
@@ -1,43 +1,14 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using UnityEngine;
 
 namespace NoAprilFools
 {
     [BepInPlugin("monky.noaprilfools", "NoAprilFools", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-
-        void Start()
+        public void Awake()
         {
-            
-            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
-        }
-
-        void OnGameInitialized()
-        {
-            GameObject April;
-            April = VRRig.LocalRig.transform.GetChild(13).gameObject; 
-            if (April.transform.name == "AprilFools") 
-            { 
-                April.SetActive(false); 
-            }
-        }
-
-        void Update()
-        {
-            foreach (VRRig vrig in GorillaParent.instance.vrrigs)
-            {
-                if (!vrig.isLocal)
-                {
-                    GameObject April;
-                    April = vrig.transform.GetChild(13).gameObject;
-                    if (April.transform.name == "AprilFools")
-                    {
-                        April.SetActive(false);
-                    }
-                }
-            }
+            Harmony.CreateAndPatchAll(GetType().Assembly, "monky.noaprilfools");
         }
     }
 }
