4504a6984e7fe5e19414ba02c0a563dbef5884db
MonkyGT <monkygtstudios@gmail.com>
2025-04-01 13:29:14 -0400

    Update Version

--- NoAprilFools/Plugin.cs
+++ NoAprilFools/Plugin.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 
 namespace NoAprilFools
 {
-    [BepInPlugin("monky.noaprilfools", "NoAprilFools", "1.0.0")]
+    [BepInPlugin("monky.noaprilfools", "NoAprilFools", "1.0.2")]
     public class Plugin : BaseUnityPlugin
     {
         public void Awake()
